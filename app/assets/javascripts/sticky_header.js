3f88e7e3ec5a56bacfc1d8ab39dcb831aad407d8
Yusuke Murakami <tropical.inu@gmail.com>
2018-09-27 11:30:59 +0900

    change_front

---
+++ app/assets/javascripts/sticky_header.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.page-header').each(function(){
+  	var $window = $(window),
+  	    $header = $(this),
+  	    headerOffsetTop = $header.offset().top;
+
+  	$window.on('scroll', function(){
+  		if ($window.scrollTop() > headerOffsetTop){
+  			$header.addClass('sticky');
+  		} else {
+  			$header.removeClass('sticky');
+  		}
+  	});
+  	 $window.trigger('scroll');
+  });
+});
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,7 @@ class BlogsController < ApplicationController
 	before_action :move_to_index, except: [:index, :show]
 
 	  def index
-		 @blogs = Blog.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+		 @blogs = Blog.includes(:user).order("created_at DESC").find(1,3,5)
     end
 
     def new
